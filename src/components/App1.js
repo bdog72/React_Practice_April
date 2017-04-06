a21c6a44542d060365b5f1a9cdc8b2a0eb8c8d3f
Brian Bycynski <high5bri@hotmail.com>
2017-04-06 06:43:45 -0400

    Made a few changes

--- src/components/App1.js
+++ src/components/App1.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class App1 extends Component {
   render () {
     function Welcome (props) {
-      return <h3>Hello, {props.name}</h3>
+      return <h4>Hello, {props.name}</h4>
     }
     return <div>
       <div>
-        <Welcome name='Sara' />
-        <Welcome name='Cahal' />
-        <Welcome name='Edite' />
+        <Welcome name='Brian' />
+        <Welcome name='Sarah' />
+        <Welcome name='Marci' />
       </div>
     </div>
   }
