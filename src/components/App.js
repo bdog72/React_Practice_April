afb72adb7d18acd17a54714a7490df58e98b48c1
Brian Bycynski <high5bri@hotmail.com>
2017-04-08 08:53:28 -0400

    added another example

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,8 @@ import Essay from './Essay'
 import FlavorForm from './FlavorForm'
 import Reservation from './Reservation'
 import Calculator from './Calculator'
+import Calculator1 from './Calculator1'
+
 import { Image, Jumbotron } from 'react-bootstrap'
 
 class App extends Component {
@@ -18,27 +20,57 @@ class App extends Component {
         <h1>Responsive Image with Bootstrap</h1>
         <Image className='image1' src='https://s-media-cache-ak0.pinimg.com/736x/ac/6e/2a/ac6e2a153df28eb73cf33921e61ca7a1.jpg' responsive />
       </Jumbotron>
-      <App1 />
-      <Clock />
-      <Toggle />
-      <br />
-      <Warning />
-      <br />
-      <Practice />
-      <br />
-      <h1>Type a name and hit submit</h1>
-      <NameForm />
-      <br />
-      <Essay />
+      <div className='border'>
+        <App1 />
+      </div>
       <br />
       <br />
-      <FlavorForm />
+      <div className='border'>
+        <Clock />
+      </div>
       <br />
       <br />
-      <Reservation />
+      <div className='border'>
+        <Toggle />
+      </div>
       <br />
       <br />
-      <Calculator />
+      <div className='border'>
+        <Warning />
+      </div>
+      <br />
+      <div className='border'>
+        <Practice />
+      </div>
+      <br />
+      <div className='border'>
+        <h1>Type a name and hit submit</h1>
+        <NameForm />
+      </div>
+      <br />
+      <div className='border'>
+        <Essay />
+      </div>
+      <br />
+      <br />
+      <div className='border'>
+        <FlavorForm />
+      </div>
+      <br />
+      <br />
+      <div className='border'>
+        <Reservation />
+      </div>
+      <br />
+      <br />
+      <div className='border'>
+        <Calculator />
+      </div>
+      <br />
+      <br />
+      <div className='border'>
+        <Calculator1 />
+      </div>
     </div>
   }
 }
---
+++ src/components/Calculator1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Calculator1 extends Component {
+  render () {
+    return <div>
+      Bdog
+    </div>
+  }
+}
